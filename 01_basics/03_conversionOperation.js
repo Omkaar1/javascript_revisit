5ce2ec47c18931bd5577a7b340f2397b47f8b527
Omkar Londhe <87084951+Omkaar1@users.noreply.github.com>
2025-01-21 10:48:02 +0000

    conversion of 3 datatypes number,string,boolean

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+let valueNumber = Number(score)
+console.log(valueNumber)
+console.log(typeof valueNumber)
+
+/* "33" => 33
+   "33abc => NaN"
+    true => 1; false => 0
+*/
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+/*
+ 1 => true; 0=> false
+ "" => false
+ "hitesh" => true
+*/
+
+let someNumber = false
+let StringSomeNumber = String(someNumber)
+console.log(StringSomeNumber)
+console.log(typeof StringSomeNumber)
+
+/*
+ 33 => string
+ string => true; string => false
+*/
