f82b2006e86a8edfd2f46528bfe0f2f35deb9c51
Omkar Londhe <87084951+Omkaar1@users.noreply.github.com>
2025-01-22 09:20:49 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,12 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(Math.floor(Date.now()/1000));
+
+console.log(myDate.getDate())
