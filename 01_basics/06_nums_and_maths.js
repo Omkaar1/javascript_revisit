00b1f439ace251b9e44a4623f45d8c38ccece519
Omkar Londhe <87084951+Omkaar1@users.noreply.github.com>
2025-01-22 08:35:55 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,24 @@
+let score = 400
+console.log(score);
+
+
+let balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const otherNumber = 12.456
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+
+console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString());
+
+
+// +++++++++++++++++ Maths +++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(3.6));
